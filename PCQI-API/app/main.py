a62b5244cd6d9b56a513a507ff52e6e93e866e70
José Miguel da Silva <jmigueldasilva953@gmail.com>
2025-09-09 21:56:15 -0300

    cors correção

--- PCQI-API/app/main.py
+++ PCQI-API/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app import models
 from app.database import engine
-from app.routers import auth, machines 
+from app.routers import auth, machines
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,6 +10,22 @@ app = FastAPI(
     title="PCQI API",
     description="API para a Plataforma de Controle de Qualidade Inteligente.",
     version="1.0.0"
+)
+
+origins = [
+    "https://pcqi.onrender.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
