81aa248d8b380606abd835e5dee52dd64a2bacc4
jcfern87 <136532734+jcfern87@users.noreply.github.com>
2025-09-18 19:57:16 -0300

    Cadastro #5
    Foram adicionados os arquivos JS de cadastro e login para testes, ambos funcionando sem erros. Também foi inserida a outra parte do HTML com a área de login, e a separação das duas (login e cadastro). A próxima atividade é criar um "separador" que cobre um dos espaços dependendo do objetivo da pessoa inserindo seus dados, e pode deslizar pro outro lado com o toque de um botão.

--- PCQI-SITE/scripts/auth/cadastro.js
+++ PCQI-SITE/scripts/auth/cadastro.js
@@ -1,5 +1,5 @@
 // Primeiro, a gente pega o formulario do nosso HTML usando o id
-const form = document.getElementById('formCadastro');
+const form = document.getElementById("formCadastro");
 
 /* Padronizar os headers para requisicao da API
 const header = new Headers();
@@ -7,70 +7,70 @@ header.append("Content-Type", "application/json");
 */
 
 // Depois, a gente adiciona um ouvinte para o evento 'submit'
-form.addEventListener('submit', async function(event) {
-    // cancela o tratamento padrao do evento
-    event.preventDefault();
+form.addEventListener("submit", async function (event) {
+  // cancela o tratamento padrao do evento
+  event.preventDefault();
 
-    // pega os dados do formulario
-    const nome = document.getElementById('nome').value;
-    const senha = document.getElementById('senha').value;
+  // pega os dados do formulario
+  const nome = document.getElementById("nomeCadastro").value;
+  const senha = document.getElementById("senhaCadastro").value;
 
-    // Validação simples
-    if (!nome || !senha) {
-      mostrarNotificacao("Preencha todos os campos!");
-      return;
+  // Validação simples
+  if (!nome || !senha) {
+    mostrarNotificacao("Preencha todos os campos!");
+    return;
+  }
+
+  // prepara os dados para envio
+  const data = {
+    nome: nome,
+    senha: senha,
+  };
+  const JSONdata = JSON.stringify(data);
+
+  try {
+    // monta e faz a requisicao
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }, // aqui mudaria para header se fosse padronizado
+      body: JSONdata,
+    });
+
+    // pega a resposta da API em json
+    const APIresponse = await response.json();
+    if (response.ok) {
+      // popa notificacao de sucesso
+      //mostrarNotificacao(APIresponse.message, true);
+      mostrarNotificacao("teste", true);
+
+      // faz o slide para login
+    } else {
+      // mensagem de erro
+      mostrarNotificacao(
+        APIresponse.message || "Erro no cadastro. Tente novamente."
+      );
     }
-
-    // prepara os dados para envio
-    const data = {
-        nome : nome,
-        senha : senha
-    }
-    const JSONdata = JSON.stringify(data);
-    
-    try {
-        // monta e faz a requisicao
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" }, // aqui mudaria para header se fosse padronizado
-            body: JSONdata
-        });
-
-        // pega a resposta da API em json
-        const APIresponse = await response.json();
-        if (response.ok) {
-            // popa notificacao de sucesso
-            //mostrarNotificacao(APIresponse.message, true);
-            mostrarNotificacao("teste", true);
-
-            // faz o slide para login
-        } else {
-            // mensagem de erro
-            mostrarNotificacao(APIresponse.message || "Erro no cadastro. Tente novamente.");
-        }
-    } catch (error) {
-      mostrarNotificacao("Erro de conexão com o servidor.");
-      console.error("Erro de rede:", error);
-    }
-
+  } catch (error) {
+    mostrarNotificacao("Erro de conexão com o servidor.");
+    console.error("Erro de rede:", error);
+  }
 });
 
 function mostrarNotificacao(mensagem, sucess = false) {
-    const notificacao = document.getElementById('notificacao');
-    notificacao.textContent = mensagem;
+  const notificacao = document.getElementById("notificacao");
+  notificacao.textContent = mensagem;
 
-    if (sucess){
-        notificacao.style.backgroundColor = "green";
-    }
-    else {
-        notificacao.style.backgroundColor = "red";
-    }
+  if (sucess) {
+    notificacao.style.backgroundColor = "green";
+  } else {
+    notificacao.style.backgroundColor = "red";
+  }
 
-    // Mostra
-    notificacao.classList.add('show');
+  // Mostra
+  notificacao.classList.add("show");
 
-    // Esconde depois de 3 segundos
-    setTimeout(() => {
-        notificacao.classList.remove('show');
-    }, 3000);
+  // Esconde depois de 3 segundos
+  setTimeout(() => {
+    notificacao.classList.remove("show");
+  }, 3000);
 }
