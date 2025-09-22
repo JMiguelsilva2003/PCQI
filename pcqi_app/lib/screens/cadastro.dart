537cdd61ffd37db854f6b1541a7c7860592f713b
Gustavo França <gustavofr0411@gmail.com>
2025-09-22 12:30:54 -0300

    Adição do campo de texto "Nome" na tela de cadastro

--- pcqi_app/lib/screens/cadastro.dart
+++ pcqi_app/lib/screens/cadastro.dart
@@ -13,10 +13,12 @@ class Cadastro extends StatefulWidget {
 
 class _CadastroState extends State<Cadastro> {
   final GlobalKey<FormState> formKeyCadastro = GlobalKey<FormState>();
+  final TextEditingController inputControllerNome = TextEditingController();
   final TextEditingController inputControllerEmail = TextEditingController();
   final TextEditingController inputControllerSenha = TextEditingController();
   final TextEditingController inputControllerSenhaConfirmacao =
       TextEditingController();
+  final FocusNode focusNodeNome = FocusNode();
   final FocusNode focusNodeEmail = FocusNode();
   final FocusNode focusNodeSenha = FocusNode();
   final FocusNode focusNodeSenhaConfirmacao = FocusNode();
@@ -27,148 +29,195 @@ class _CadastroState extends State<Cadastro> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.branco,
+      resizeToAvoidBottomInset: true,
       body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(30),
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Título
-                  Text("Cadastre-se", style: AppStyles.textStyleTitulo),
+          ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).viewInsets.top + 150,
+                      ),
+                      // Título
+                      Text("Cadastre-se", style: AppStyles.textStyleTitulo),
 
-                  // Espaço
-                  const SizedBox(height: 40),
+                      // Espaço
+                      const SizedBox(height: 40),
 
-                  // Formulário
-                  Form(
-                    key: formKeyCadastro,
-                    child: Column(
-                      children: [
-                        // Email widget
-                        TextFormField(
-                          controller: inputControllerEmail,
-                          focusNode: focusNodeEmail,
-                          buildCounter:
-                              (
-                                BuildContext context, {
-                                int? currentLength,
-                                int? maxLength,
-                                bool? isFocused,
-                              }) => null,
-                          style: AppStyles.textFieldTextStyle,
-                          decoration:
-                              AppStyles.textFieldDecoration(
-                                "Endereço de e-mail",
-                              ).copyWith(
-                                prefixIcon: Icon(
-                                  Icons.mail,
-                                  color: AppColors.cinza,
-                                ),
-                              ),
-                          textInputAction: TextInputAction.next,
-                          validator: verificarCampoEmail,
-                          onChanged: onChangedForm,
-                          onFieldSubmitted: (_) {
-                            FocusScope.of(context).requestFocus(focusNodeSenha);
-                          },
-                        ),
-
-                        // Espaço
-                        const SizedBox(height: 15),
-
-                        // Widget de senha
-                        TextFormField(
-                          controller: inputControllerSenha,
-                          focusNode: focusNodeSenha,
-                          obscureText: visibilidadeSenha,
-                          autofillHints: [AutofillHints.password],
-                          buildCounter:
-                              (
-                                BuildContext context, {
-                                int? currentLength,
-                                int? maxLength,
-                                bool? isFocused,
-                              }) => null,
-                          maxLength: 20,
-                          decoration: AppStyles.textFieldDecoration('Senha')
-                              .copyWith(
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  color: AppColors.cinza,
-                                ),
-
-                                // Exibe/oculta senha
-                                suffixIcon: IconButton(
-                                  icon: visibilidadeSenha
-                                      ? Icon(Icons.visibility_off)
-                                      : Icon(Icons.visibility),
-                                  onPressed: () => setState(
-                                    () =>
-                                        visibilidadeSenha = !visibilidadeSenha,
+                      // Formulário
+                      Form(
+                        key: formKeyCadastro,
+                        child: Column(
+                          children: [
+                            // Widget de nome
+                            TextFormField(
+                              controller: inputControllerNome,
+                              focusNode: focusNodeNome,
+                              autofillHints: [AutofillHints.name],
+                              buildCounter:
+                                  (
+                                    BuildContext context, {
+                                    int? currentLength,
+                                    int? maxLength,
+                                    bool? isFocused,
+                                  }) => null,
+                              maxLength: 80,
+                              decoration: AppStyles.textFieldDecoration('Nome')
+                                  .copyWith(
+                                    prefixIcon: Icon(
+                                      Icons.person,
+                                      color: AppColors.cinza,
+                                    ),
                                   ),
-                                ),
-                              ),
-                          style: AppStyles.textFieldTextStyle,
-                          textInputAction: TextInputAction.next,
-                          validator: verificarCampoSenha,
-                          onChanged: onChangedForm,
-                          onFieldSubmitted: (_) {
-                            FocusScope.of(
-                              context,
-                            ).requestFocus(focusNodeSenhaConfirmacao);
-                          },
-                        ),
+                              validator: verificarCampoNome,
+                              onChanged: onChangedForm,
+                              style: AppStyles.textFieldTextStyle,
+                              textInputAction: TextInputAction.next,
+                              onFieldSubmitted: (_) {
+                                FocusScope.of(
+                                  context,
+                                ).requestFocus(focusNodeEmail);
+                              },
+                            ),
 
-                        // Espaço
-                        const SizedBox(height: 15),
+                            // Espaço
+                            const SizedBox(height: 15),
 
-                        // Widget de confirmação de senha
-                        TextFormField(
-                          controller: inputControllerSenhaConfirmacao,
-                          focusNode: focusNodeSenhaConfirmacao,
-                          obscureText: visibilidadeSenha,
-                          autofillHints: [AutofillHints.password],
-                          keyboardType: TextInputType.visiblePassword,
-                          buildCounter:
-                              (
-                                BuildContext context, {
-                                int? currentLength,
-                                int? maxLength,
-                                bool? isFocused,
-                              }) => null,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                            // Email widget
+                            TextFormField(
+                              controller: inputControllerEmail,
+                              focusNode: focusNodeEmail,
+                              autofillHints: [AutofillHints.email],
+                              keyboardType: TextInputType.emailAddress,
+                              buildCounter:
+                                  (
+                                    BuildContext context, {
+                                    int? currentLength,
+                                    int? maxLength,
+                                    bool? isFocused,
+                                  }) => null,
+                              style: AppStyles.textFieldTextStyle,
+                              decoration:
+                                  AppStyles.textFieldDecoration(
+                                    "Endereço de e-mail",
+                                  ).copyWith(
+                                    prefixIcon: Icon(
+                                      Icons.mail,
+                                      color: AppColors.cinza,
+                                    ),
+                                  ),
+                              textInputAction: TextInputAction.next,
+                              validator: verificarCampoEmail,
+                              onChanged: onChangedForm,
+                              onFieldSubmitted: (_) {
+                                FocusScope.of(
+                                  context,
+                                ).requestFocus(focusNodeSenha);
+                              },
+                            ),
+
+                            // Espaço
+                            const SizedBox(height: 15),
+
+                            // Widget de senha
+                            TextFormField(
+                              controller: inputControllerSenha,
+                              focusNode: focusNodeSenha,
+                              obscureText: visibilidadeSenha,
+                              autofillHints: [AutofillHints.password],
+                              keyboardType: TextInputType.visiblePassword,
+                              buildCounter:
+                                  (
+                                    BuildContext context, {
+                                    int? currentLength,
+                                    int? maxLength,
+                                    bool? isFocused,
+                                  }) => null,
+                              maxLength: 20,
+                              decoration: AppStyles.textFieldDecoration('Senha')
+                                  .copyWith(
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      color: AppColors.cinza,
+                                    ),
+
+                                    // Exibe/oculta senha
+                                    suffixIcon: IconButton(
+                                      icon: visibilidadeSenha
+                                          ? Icon(Icons.visibility_off)
+                                          : Icon(Icons.visibility),
+                                      onPressed: () => setState(
+                                        () => visibilidadeSenha =
+                                            !visibilidadeSenha,
+                                      ),
+                                    ),
+                                  ),
+                              style: AppStyles.textFieldTextStyle,
+                              textInputAction: TextInputAction.next,
+                              validator: verificarCampoSenha,
+                              onChanged: onChangedForm,
+                              onFieldSubmitted: (_) {
+                                FocusScope.of(
+                                  context,
+                                ).requestFocus(focusNodeSenhaConfirmacao);
+                              },
+                            ),
+
+                            // Espaço
+                            const SizedBox(height: 15),
+
+                            // Widget de confirmação de senha
+                            TextFormField(
+                              controller: inputControllerSenhaConfirmacao,
+                              focusNode: focusNodeSenhaConfirmacao,
+                              obscureText: visibilidadeSenha,
+                              autofillHints: [AutofillHints.password],
+                              keyboardType: TextInputType.visiblePassword,
+                              buildCounter:
+                                  (
+                                    BuildContext context, {
+                                    int? currentLength,
+                                    int? maxLength,
+                                    bool? isFocused,
+                                  }) => null,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                              ],
+                              maxLength: 20,
+                              decoration:
+                                  AppStyles.textFieldDecoration(
+                                    'Confirme sua senha',
+                                  ).copyWith(
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      color: AppColors.cinza,
+                                    ),
+                                  ),
+                              textInputAction: TextInputAction.done,
+                              style: AppStyles.textFieldTextStyle,
+                              validator: verificarCampoSenhaConfirmacao,
+                              onChanged: onChangedForm,
+                            ),
+
+                            // Espaço
+                            const SizedBox(height: 15),
+
+                            // Widget do botão de cadastro
+                            construirBotaoCadastro(),
                           ],
-                          maxLength: 20,
-                          decoration:
-                              AppStyles.textFieldDecoration(
-                                'Confirme sua senha',
-                              ).copyWith(
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  color: AppColors.cinza,
-                                ),
-                              ),
-                          textInputAction: TextInputAction.done,
-                          style: AppStyles.textFieldTextStyle,
-                          validator: verificarCampoSenhaConfirmacao,
-                          onChanged: onChangedForm,
                         ),
-
-                        // Espaço
-                        const SizedBox(height: 15),
-
-                        // Widget do botão de cadastro
-                        construirBotaoCadastro(),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
           // Seta para voltar à tela anterior
           voltarTelaAnterior(),
@@ -184,7 +233,7 @@ class _CadastroState extends State<Cadastro> {
       type: ButtonType.elevated,
       onPressed: () async {
         // método para enviar cadastro aqui
-        verificaCamposValidos();
+        verificaCamposValidos(formKeyCadastro);
       },
       successDuration: Duration(seconds: 0),
       style: AppStyles.loadingButtonStyle,
@@ -206,7 +255,14 @@ class _CadastroState extends State<Cadastro> {
     ),
   );
 
-  /* ----- Métodos para verificar se os campos de texto estão corretamente preenchidos ----- */
+  String? verificarCampoNome(String? value) {
+    // Verifica se o campo não está vazio
+    if (value!.isEmpty) {
+      mostrarErroFormInput = true;
+      return "O campo não deve estar vazio";
+    }
+    return null;
+  }
 
   String? verificarCampoEmail(String? value) {
     final pattern =
@@ -271,8 +327,8 @@ class _CadastroState extends State<Cadastro> {
   }
 
   // Verifica se todos os campos de texto necessários para o cadastro estão preenchidos
-  bool verificaCamposValidos() {
-    return formKeyCadastro.currentState!.validate();
+  bool verificaCamposValidos(formKey) {
+    return formKey.currentState!.validate();
   }
 
   void onChangedForm(String value) {
