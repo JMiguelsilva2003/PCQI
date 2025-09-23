7947c5ccd0cb0c124c9965f06479af6317be0a3b
José Miguel da Silva <jmigueldasilva953@gmail.com>
2025-09-23 10:31:17 -0300

    Update main.py

--- PCQI-API/app/main.py
+++ PCQI-API/app/main.py
@@ -12,7 +12,11 @@ app = FastAPI(
     version="1.0.0"
 )
 
-origins = ["*"]
+origins = [
+    "https://pcqi.onrender.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
 
 app.add_middleware(
     CORSMiddleware,
