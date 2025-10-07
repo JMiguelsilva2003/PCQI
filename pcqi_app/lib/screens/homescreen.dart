44b7f1c4041bceb1d2fd990e5955df7f950420d0
Pedro  Emmanuel G. C. Machado <132278032+Pedro-Emmanuel-G-C-Machado@users.noreply.github.com>
2025-10-06 23:30:41 -0300

    Primeiros componentes e a rota para a câmera

--- pcqi_app/lib/screens/homescreen.dart
+++ pcqi_app/lib/screens/homescreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pcqi_app/config/app_colors.dart';
 import 'package:pcqi_app/config/app_styles.dart';
+import 'package:pcqi_app/screens/teste_camera.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -12,11 +14,30 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Usuário logado",
-          style: AppStyles.textStyleTituloSecundario,
-        ),
+      backgroundColor: AppColors.branco, body: Stack(
+        children:[
+          Center(
+            child:  Text("Usuário Logado"),
+          ),
+        Positioned(
+  top: 30,
+  right: 30,
+  child:FloatingActionButton.extended(
+  onPressed: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const TesteCamera()),
+    );
+  },
+  icon: const Icon(Icons.camera_alt), // ícone
+  label: const Text("Câmera"),        // texto ao lado
+  backgroundColor: Colors.blue,       // cor do botão
+  foregroundColor: Colors.white,      // cor do texto/ícone
+),
+
+),
+
+        ],
       ),
     );
   }
