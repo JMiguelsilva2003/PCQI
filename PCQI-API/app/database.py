49bb4dd1474df0c630f67984668139bee9d7a2c1
José Miguel da Silva <jmigueldasilva953@gmail.com>
2025-10-07 03:31:18 -0300

    Update database.py

--- PCQI-API/app/database.py
+++ PCQI-API/app/database.py
@@ -1,14 +1,19 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker, declarative_base 
 
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"sslmode": "require"}, 
+    pool_pre_ping=True,                   
+    pool_recycle=1800                     
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
