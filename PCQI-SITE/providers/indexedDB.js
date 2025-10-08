a239ff4c46400c04a50c9c7ba9a97f80b9b538a3
AlisonHSerpa <147287390+AlisonHSerpa@users.noreply.github.com>
2025-10-07 22:59:32 -0300

    provider iniciado, indexedDB imports feito

---
+++ PCQI-SITE/providers/indexedDB.js
@@ -0,0 +1,15 @@
+import {
+  get,
+  set,
+  getMany,
+  setMany,
+  update,
+  del,
+  clear,
+  keys,
+  values,
+  entries,
+  createStore,
+} from './import';
+
+// planejar o que salvar de dados
