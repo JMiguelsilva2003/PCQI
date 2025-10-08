81138844255418d26486fc3ec84afb1fd4a164db
AlisonHSerpa <147287390+AlisonHSerpa@users.noreply.github.com>
2025-10-07 23:03:12 -0300

    fix no nome do indexedDB

---
+++ PCQI-SITE/providers/internalDB.js
@@ -0,0 +1,15 @@
+import {
+  get,
+  set,
+  getMany,
+  setMany,
+  update,
+  del,
+  clear,
+  keys,
+  values,
+  entries,
+  createStore,
+} from './indexedDB';
+
+// planejar o que salvar de dados
