78dcc0fca8bc9ca6047a3a695d01e7b7268564c8
José Miguel da Silva <jmigueldasilva953@gmail.com>
2025-11-19 10:22:27 -0300

    testes no docker feitos

--- PCQI-SITE/tests_ui/conftest.py
+++ PCQI-SITE/tests_ui/conftest.py
@@ -2,34 +2,35 @@ import pytest
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
+CHROME_DRIVER_PATH = "/usr/bin/chromedriver" 
 
 @pytest.fixture(scope="function")
 def driver():
-    
+
     options = webdriver.ChromeOptions()
     
+    options.add_argument("--headless") 
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--start-maximized")
+    options.add_argument("--disable-dev-shm-usage") 
+    options.add_argument("--start-maximized") 
     options.add_argument("--disable-blink-features=AutomationControlled")
     
     prefs = {
-        "credentials_enable_service": False,    
+        "credentials_enable_service": False,
         "profile.password_manager_enabled": False,
         "profile.password_manager_leak_detection": False,
         "safebrowsing.enabled": False
     }
     options.add_experimental_option("prefs", prefs)
-    
     options.add_argument("--disable-session-crashed-bubble")
-    options.add_argument("--ignore-certificate-errors")
-
-    service = Service(ChromeDriverManager().install())
+    service = Service(CHROME_DRIVER_PATH) 
     driver = webdriver.Chrome(service=service, options=options)
     
     driver.implicitly_wait(5)
+    
     yield driver
+    
     driver.quit()
 
 @pytest.fixture
